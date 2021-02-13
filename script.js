3051ccebed7fdd8a81d5e5cde527d6bb9ba8ac8e
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-12 22:04:49 -0500

    tweaking the send email input

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+const formInput = document.querySelectorAll('.form-input')
 const formSubmit = document.querySelector('.send-form')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
 
-formSubmit.addEventListener('click', () => alert("Email Sent!"))
+formSubmit.addEventListener('click', () => {
+  console.log(formInput.value)
+  alert('Email Sent!')
+})
