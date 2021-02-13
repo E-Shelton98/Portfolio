dbabdc98c1982748bf86d683d5c21b1c761f6a88
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-12 21:45:35 -0500

    alert added for form submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+const formSubmit = document.querySelector('.send-form')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+formSubmit.addEventListener('click', () => alert("Email Sent!"))
