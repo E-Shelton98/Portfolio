0f2b0a3644b64f2b910a684c82f5b8b914f46498
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-13 22:38:16 -0500

    added transformation to info boxes

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+const infos = document.querySelectorAll('.info')
 const formInput = document.querySelectorAll('.form-input')
 const formSubmit = document.querySelector('.send-form')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+window.addEventListener('scroll', checkInfo)
+
+function checkInfo() {
+  const triggerBottom = (window.innerHeight / 5) * 4
+
+  infos.forEach((info) => {
+    const infoTop = info.getBoundingClientRect().top
+
+    if (infoTop < triggerBottom) {
+      info.classList.add('show')
+    }
+  })
+}
 
 formSubmit.addEventListener('click', () => {
   console.log(formInput.value)
